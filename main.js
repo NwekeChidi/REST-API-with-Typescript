a4d5b2fc2f5c701442e606c8bc85797dc2fdf757
Chidimma Dominic Nweke <realchidinweke@gmail.com>
2022-05-16 08:24:46 +0100

    variable types

--- main.js
+++ main.js
@@ -1,3 +1,39 @@
-// First line of code
-var message = "Hello World";
+"use strict";
+exports.__esModule = true;
+var message = "Welcome Back";
 console.log(message);
+// variable types
+var isBeginner = true;
+var total = 0;
+var name = "Gabriel";
+var sentence = "My name is ".concat(name, ",\nI am a beginner in Typescript");
+console.log(sentence);
+// More types
+var n = null;
+var u = undefined;
+// Declaring arrays
+var list1 = [1, 2, 3];
+var list2 = [1, 2, 3];
+var person1 = ["chris", 33];
+var Color;
+(function (Color) {
+    Color[Color["Red"] = 0] = "Red";
+    Color[Color["Green"] = 1] = "Green";
+    Color[Color["Blue"] = 2] = "Blue";
+})(Color || (Color = {}));
+;
+var c = Color.Blue;
+console.log(c);
+// any variable type
+var myVariable = 10;
+// unknown variable type
+var myVariable1 = 10;
+function hasName(obj) {
+    return !!obj &&
+        typeof obj === "object" &&
+        "name" in obj;
+}
+if (hasName(myVariable1)) {
+    console.log(myVariable1.name);
+}
+//(myVariable as string).toUpperCase();
