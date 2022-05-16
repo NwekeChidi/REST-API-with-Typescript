0017d3a51ce931a3fcddf41bca6f8c23e58089e5
Chidimma Dominic Nweke <realchidinweke@gmail.com>
2022-05-16 08:25:00 +0100

    variables types

--- Notes.md
+++ Notes.md
@@ -12,3 +12,13 @@
 - IDE support
 - Rapid growth and use
 - It is the main programming language of Angular
+
+## Variables Types
+
+- The basic variable types available in typescript include number, string, and boolean
+- null and undefined are considered secondary variable types
+- A variable of type string, boolean, or number can have null or undefined assigned to it
+- When defining a list variable of type tuple, the order at which the values are assigned **must** match the order at which the tuple is defined
+- TypeScript also has the **any** variable type which can be used to initialize variable without a specific type
+- TypeScript also has the **unknown** variable type
+  -- It also includes type casting
--- main.ts
+++ main.ts
@@ -1,3 +1,46 @@
 // First line of code
-let message = "Hello World";
+export {}
+let message = "Welcome Back";
 console.log(message);
+
+// variable types
+let isBeginner: boolean = true;
+let total: number = 0;
+let name: string = "Gabriel";
+
+let sentence: string = `My name is ${name},
+I am a beginner in Typescript`;
+
+console.log(sentence);
+
+// More types
+let n: null = null;
+let u: undefined = undefined;
+
+// Declaring arrays
+let list1: number[] = [1, 2, 3];
+let list2: Array<number> = [1, 2, 3];
+
+let person1: [string, number] = ["chris", 33];
+
+enum Color { Red, Green, Blue };
+let c: Color = Color.Blue;
+console.log(c);
+
+// any variable type
+let myVariable: any = 10;
+
+// unknown variable type
+let myVariable1: unknown = 10
+
+function hasName(obj: any): obj is { name: string } {
+    return !! obj &&
+        typeof obj === "object" &&
+        "name" in obj
+}
+
+if (hasName(myVariable1)) {
+    console.log(myVariable1.name)
+}
+
+//(myVariable as string).toUpperCase();
