50be1e526db85010a34b37a690ad1ad9dc201db5
Chidimma Dominic Nweke <realchidinweke@gmail.com>
2022-05-16 09:51:06 +0100

    classes

--- main.ts
+++ main.ts
@@ -70,3 +70,35 @@ let p = {
 }
 
 fullName(p);
+
+// Classes and Access Modifiers
+class Employee {
+    employeeName: string;
+
+    constructor(name: string) {
+        this.employeeName = name;
+    }
+
+    greet() {
+        console.log(`Good Day ${this.employeeName}`);
+    }
+}
+
+let emp1 = new Employee("Gabriel");
+console.log(emp1.employeeName)
+emp1.greet();
+
+class Manager extends Employee {
+    constructor(managerName: string) {
+        super(managerName);
+    }
+
+    delegateWork() {
+        console.log("Manager delegating tasks");
+    }
+}
+
+let m1 = new Manager("Bruce");
+console.log(); console.log(m1.employeeName);
+m1.greet();
+m1.delegateWork()
